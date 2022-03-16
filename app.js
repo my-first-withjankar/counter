294ac6ec656c1bff86ef887196d0eace73261cfa
jankar-with-tarek <avirus170@gmail.com>
2022-03-16 17:38:56 +0600

    counter done

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ btns.forEach(function (btn) {
         else {
             count = 0;
         }
+        if (count < 0) {
+            value.style.color = 'red';
+        }
+        if (count > 0) {
+            value.style.color = 'green'
+        }
+        if (count === 0) {
+            value.style.color = 'gray'
+        }
+
         value.textContent = count;
     })
 })
